66fb409ce12d779ab5b77067f85233637e1e2052
Carlos <carloxdev@gmail.com>
2017-10-11 18:14:39 -0500

    Fix pace

--- home/static/master.js
+++ home/static/master.js
@@ -1,13 +1,18 @@
 
 /* ------------------------ LOAD ------------------------ */
 
-$(document).ready(function () {
-    window.paceOptions = {
-        // Disable the 'elements' source
-        elements: false,
-        // Only show the progress on regular and ajax-y page navigation,
-        // not every request
-        restartOnRequestAfter: false,
-        startOnPageLoad: false,
+window.paceOptions = {
+    // Disable the 'elements' source
+    elements: false,
+    // Only show the progress on regular and ajax-y page navigation,
+    // not every request
+    restartOnRequestAfter: false,
+    startOnPageLoad: false,
+    ajax: {
+      trackMethods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH']
     }
-})
+}
+
+// $(document).ready(function () {
+//
+// })
